4a5cb7594b1bbb4139ba8fbe320f257dd2f0a9f1
Sunil Shaw <34816778+erronny@users.noreply.github.com>
2021-02-17 11:38:45 +0530

    Add files via upload

---
+++ categories/models.py
@@ -0,0 +1,64 @@
+'''menu models.'''
+from django.db import models
+from django_extensions.db.fields import AutoSlugField
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from wagtail.admin.edit_handlers import (
+    MultiFieldPanel,
+    FieldPanel,
+    InlinePanel,
+    PageChooserPanel,
+)
+from wagtail.core.models import Orderable
+from wagtail.snippets.models import register_snippet
+
+
+class MenuItem(Orderable):
+    link_title = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50
+    )
+    link_url = models.CharField(
+        max_length=500,
+        blank=True
+    )
+    link_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.CASCADE,
+    )
+    open_in_new_tap = models.BooleanField(default=False, blank=True)
+
+    page = ParentalKey("Menu", related_name="menu_items")
+
+    panel = [
+        FieldPanel("link_title"),
+        FieldPanel("link_url"),
+        PageChooserPanel("link_page"),
+        FieldPanel("open_in_new_tab"),
+
+    ]
+    # @todo ad properties
+    # link
+    # title
+    #
+
+    @register_snippet
+    class Menu(ClusterableModel):
+        title = models.CharField(max_length=100)
+        slug = AutoSlugField(populate_from="title", editable=True)
+
+
+        panels = [
+            MultiFieldPanel([
+                FieldPanel("title"),
+                FieldPanel("slug"),
+            ], heading="Menu"),
+            InlinePanel("menu_items", label="Menu Item")
+        ]
+
+        def __str__(self):
+            return self.title
---
+++ contact/models.py
@@ -0,0 +1,66 @@
+from django.forms import widgets
+
+
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel
+)
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+
+class ContactPage(AbstractEmailForm):
+
+    template = "contact/contact_page.html"
+    # This is the default path.
+    # If ignored, Wagtail adds _landing.html to your template name
+    landing_page_template = "contact/contact_page_landing.html"
+
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
+    ]
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        # form = super(AbstractEmailForm, self).get_form(*args, **kwargs)  # use this syntax for Python 2.x
+        # iterate through the fields in the generated form
+        for name, field in form.fields.items():
+            # here we want to adjust the widgets on each field
+            # if the field is a TextArea - adjust the rows
+            if isinstance(field.widget, widgets.Textarea):
+                field.widget.attrs.update({'rows': '5'})
+            # for all fields, get any existing CSS classes and add 'form-control'
+            # ensure the 'class' attribute is a string of classes with spaces
+            css_classes = field.widget.attrs.get('class', '').split()
+            css_classes.append('form-control')
+            field.widget.attrs.update({'class': ' '.join(css_classes)})
+        return form
+
